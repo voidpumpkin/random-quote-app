bb3a41f98aa7867aa67ad4299688d61df7551e26
Voidpumpkin <juliuslungys@gmail.com>
2019-05-11 16:32:57 +0300

    add css-loader node-sass style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -12,9 +12,29 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.html$/,
-            use: ['html-loader']
-        }]
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(scss)$/,
+                use: [{
+                    loader: 'style-loader', // inject CSS to page
+                }, {
+                    loader: 'css-loader', // translates CSS into CommonJS modules
+                }, {
+                    loader: 'postcss-loader', // Run postcss actions
+                    options: {
+                        plugins: function () { // postcss plugins, can be exported to postcss.config.js
+                            return [
+                                require('autoprefixer')
+                            ];
+                        }
+                    }
+                }, {
+                    loader: 'sass-loader' // compiles Sass to CSS
+                }]
+            }
+        ]
     },
     plugins: [
         new HtmlWebpackPlugin({
