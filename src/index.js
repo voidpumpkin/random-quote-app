d1dc89552597936b1902e7ca2a8925a815284223
Voidpumpkin <juliuslungys@gmail.com>
2019-07-16 15:35:34 +0300

    fix image fetching

--- src/index.js
+++ src/index.js
@@ -1,102 +1,114 @@
-import * as $ from 'jquery';
-import '../scss/custom.scss';
-import 'bootstrap';
-import * as image from '../images/default.jpg';
-import generateAnimeQuoute from 'animequote';
+import * as $ from "jquery";
+import "../scss/custom.scss";
+import "bootstrap";
+import * as defaultAuthorImage from "../images/default.jpg";
+import generateAnimeQuote from "animequote";
 
 let isImageLoading = false;
 
 (function init() {
-    $('#new-quote').on('click', (event) => {
-        event.preventDefault();
-        if (!isImageLoading) {
-            changeQoute();
-        }
-    });
-    changeQoute();
+  addQuoteNewClickEvent();
+  changeQuote();
 })();
 
-async function changeQoute() {
-    let animeQoute = generateAnimeQuoute();
-    $('#quote-text').text(animeQoute.quotesentence);
-    $('#author').text(`- ${animeQoute.quotecharacter}`);
-
-    $('#author-image').remove();
-    addLoading();
-
-    let imageUrl = undefined;
-    try {
-        imageUrl = await getCharacterImageUrl(animeQoute.quoteanime, animeQoute.quotecharacter);
-    } catch (error) {
-        console.log('there was an error');
-    } finally {
-        $('#author-image-loading').remove();
-        isImageLoading = false;
-        addDefaultAuthorImage();
-        if (imageUrl) {
-            $('#author-image').attr('src', imageUrl);
+function addQuoteNewClickEvent() {
+    $("#quote-new").on("click", event => {
+        event.preventDefault();
+        if (!isImageLoading) {
+            changeQuote();
         }
-    }
+    });
 }
 
-function addDefaultAuthorImage() {
-    let html = '<img id="author-image" class="author-image d-block mx-auto pr-3 oppacity-60" ' +
-        `src = "${image}" alt = "Author image" >`;
-    $('#quote').prepend(html);
+async function changeQuote() {
+  let animeQuote = generateAnimeQuote();
+  changeSentence(animeQuote.quotesentence);
+  changeAuthor(animeQuote.quotecharacter);
+  await changeAuthorImageBy(animeQuote.quoteanime, animeQuote.quotecharacter);
+}
+
+async function changeAuthorImageBy(anime, character) {
+  $("#author-image").remove();
+  addLoading();
+  let imageUrl = undefined;
+  try {
+    imageUrl = await getCharacterImageUrlBy(anime, character);
+  } catch (error) {
+    console.warn("Could not get character image because:\n",error);
+  } finally {
+    removeLoading();
+    addDefaultAuthorImage(imageUrl);
+  }
+}
+
+function removeLoading() {
+  $("#author-image-loading").remove();
+  isImageLoading = false;
+}
+
+function changeAuthor(quotecharacter) {
+  $("#quote-author").text(`- ${quotecharacter}`);
+}
+
+function changeSentence(quotesentence) {
+  $("#quote-text").text(quotesentence);
+}
+
+function addDefaultAuthorImage(imageUrl) {
+  let html =
+    '<img id="author-image" class="author-image d-block mx-auto pr-3 oppacity-60" ' +
+    `src = "${
+      imageUrl ? imageUrl : defaultAuthorImage
+    }" alt = "Author image" >`;
+  $("#quote").prepend(html);
 }
 
 function addLoading() {
-    let html =
-      `<div id="author-image-loading" class="d-flex mx-auto author-image pr-3 oppacity-60 box-sizing-content" style="width: ${vhToPixels(45) * 0.643759}px;">`+
-      '<div class="spinner-grow mx-auto d-block align-self-center justify-self-center" role="status" >' +
-      '<span class="sr-only">Loading...</span>' +
-      "</div>"+
-      "</div>";
-    $("#quote").prepend(html);
-    isImageLoading = true;
-    }
+  let html =
+    `<div id="author-image-loading" class="d-flex mx-auto author-image pr-3 oppacity-60 box-sizing-content" style="width: ${vhToPixels(
+      45
+    ) * 0.643759}px;">` +
+    '<div class="spinner-grow mx-auto d-block align-self-center justify-self-center" role="status" >' +
+    '<span class="sr-only">Loading...</span>' +
+    "</div>" +
+    "</div>";
+  $("#quote").prepend(html);
+  isImageLoading = true;
+}
 
-async function getCharacterImageUrl(anime, character) {
-    let characterNameBits = character.split(" ");
-    let animeData = await searchForAnime(anime);
-    //TODO: fix this
-    if (animeData) {
-        let characters = await fetchAnimeCharacters(animeData.results[0].mal_id);
-        if (characters) {
-            let matchingCharacters = characters.characters.filter(
-                e =>
-                e.name.includes(characterNameBits[0]) &&
-                e.name.includes(characterNameBits[characterNameBits.length - 1])
-            );
-            return matchingCharacters[0].image_url;
-        }
-    }
+async function getCharacterImageUrlBy(anime, character) {
+  let characterNameBits = character.split(" ");
+  let animeData = await fetchSearchForAnime(anime);
+  let characters = await fetchAnimeCharacters(animeData.mal_id);
+  let matchingCharacters = characters.filter(
+    e =>
+      e.name.includes(characterNameBits[0]) &&
+      e.name.includes(characterNameBits[characterNameBits.length - 1])
+  );
+  return matchingCharacters[0].image_url;
 }
 
 async function fetchAnimeCharacters(animeId) {
-    let characters;
-    characters = $.ajax({
-        url: `https://api.jikan.moe/v3/anime/${animeId}/characters_staff`,
-        type: 'GET'
-    });
-    return characters;
+  let response = await $.ajax({
+    url: `https://api.jikan.moe/v3/anime/${animeId}/characters_staff`,
+    type: "GET"
+  });
+  return response.characters;
 }
 
-async function searchForAnime(anime) {
-    let animeData;
-    animeData = $.ajax({
-        url: 'https://api.jikan.moe/v3/search/anime',
-        type: 'GET',
-        data: {
-            type: 'anime',
-            q: anime,
-            limit: 1
-        }
-    });
-    //TODO: should return.results[0]
-    return animeData;
+async function fetchSearchForAnime(anime) {
+  let response = await $.ajax({
+    url: "https://api.jikan.moe/v3/search/anime",
+    type: "GET",
+    data: {
+      type: "anime",
+      q: anime,
+      limit: 1
+    }
+  });
+  return response.results[0];
 }
 
-function vhToPixels (vh) {
-    return Math.round(window.innerHeight / (100 / vh));
-  }
+function vhToPixels(vh) {
+  return Math.round(window.innerHeight / (100 / vh));
+}
